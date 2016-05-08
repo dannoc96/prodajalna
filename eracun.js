b2b12af531a1cc45e913eaa0c789019392c3661e
Dan <dannoc1996@gmail.com>
2016-05-08 19:06:23 +0000

    Zaključek dela na II. nalogi

--- eracun.js
+++ eracun.js
@@ -237,7 +237,7 @@ streznik.post('/stranka', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
-    zahteva.session.str = true;
+    zahteva.session.str = polja.seznamStrank;
     odgovor.redirect('/')
   });
 })
